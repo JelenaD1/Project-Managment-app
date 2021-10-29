02c3aa4a37a7a2efecba1661912782e2851b61ed
Jelena <jelena.durovic@powerhrg.com>
2021-10-29 12:08:58 -0400

    show project feature spec done

--- spec/system/projects/show_spec.rb
+++ spec/system/projects/show_spec.rb
@@ -34,4 +34,24 @@ describe "when user visits the project show page" do
     expect(page.current_path).to eq new_project_task_path(project)
   end
 
+  it "has a list of projects tasks", :aggregate_failures do
+    project = create(:project, user: @user, name: "JavaScript is my nightmare")
+    task = create(:task, project: project, title: "Need a set up")
+
+    visit project_path(project)
+
+    expect(page).to have_content "JavaScript is my nightmare"
+    expect(page).to have_content "Need a set up"
+  end
+
+  it "has a edit button to edit the task", :aggregate_failures do
+    project = create(:project, user: @user, name: "JavaScript is my nightmare")
+    task = create(:task, project: project, title: "Need a set up")
+
+    visit project_path(project)
+
+    click_on(id: "edit task")
+
+    expect(page.current_path).to eq edit_project_task_path(task, project)
+  end
 end
