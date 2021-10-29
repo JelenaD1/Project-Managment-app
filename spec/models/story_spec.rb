ecfbad197a44672b0c2cac9c4ad303a02ce388a8
Jelena <jelena.durovic@powerhrg.com>
2021-10-29 13:25:13 -0400

    done

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe Story, type: :model do
   it { should have_many(:mentors)}
   it { should validate_presence_of(:name)}
 
+  describe ".search" do
+    before do
+      @user = create(:user)
+    end
+
+    it "finds a searched story by it's name" do
+      story = create(:story, user: @user, name: "Nav bar")
+      result = Story.search("Nav bar")
+      expect(result).to include(story)
+    end
+
+    it "finds all stories if argument is not given" do
+      story = create(:story, user: @user, name: "Nav bar")
+      result = Story.search(nil)
+      expect(result).to include(story)
+    end
+  end
+
 end
